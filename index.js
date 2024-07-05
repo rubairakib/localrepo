bf0f97f31b467406da5f65d6725791b3ddc027ec
rubairakib <rubairakib11@gmail.com>
2024-07-05 18:29:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('hiteshdotcom')
+})
+
+app.get('/login', (req, res) =>{
+    res.send('<h1>please login at chai aur code</h1>')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send('<h2>chai aur code</h2>')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
